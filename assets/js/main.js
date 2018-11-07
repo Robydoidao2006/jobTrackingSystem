a3c2e9fb3d6e06da1552019d435442dcd76b26ac
robert Fialho <32443950+Robydoidao2006@users.noreply.github.com>
2018-11-06 21:41:30 -0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,14 @@
-// Submit event
+
+// Runs app
 function display(){
 
-    const companyName = document.querySelector('#companyNameDiv').value 
-    document.getElementById('test1').innerHTML = companyName;
-
-    const jobLocation = document.querySelector('#jobLocationDiv').value  
-    document.getElementById('test2').innerHTML = jobLocation;
-
-    // const postingWebsite = document.querySelector('#postingWebsiteDiv').value
-    // document.getElementById('postingWebsiteDiv').innerHTML = postingWebsite;
-
-
-    // var x = document.getElementById("postingWebsite").selectedIndex;
-    // (document.getElementsByTagName("option")[x].value).innerHTML;
-
-    current_date();
+    grabInput();
+    currentDate();
 }
 
 
-// Display current date on submition
-function current_date(){
+// Display current date on submition.
+function currentDate(){
 
     const todayTime = new Date();
     const month = (todayTime .getMonth() + 1);
@@ -28,8 +17,27 @@ function current_date(){
     const hour = (todayTime .getHours());
     const min = ('0' + todayTime.getMinutes()).slice(-2);
     
-    document.getElementById("current-date-Div").innerHTML = (month + "/" + day + "/" + year + " " + hour +":"+ min);
+    document.getElementById("currentDateDiv").innerHTML += '<p>' + month + "/" + day + "/" + year + " " + hour +":"+ min + '</p>';
 
 }
 
+// Grabs user input and places it in a div. 
+function grabInput (){
 
+    const companyName = document.querySelector('#companyNameInput').value 
+    document.getElementById('companyNameDiv').innerHTML += '<p>' + companyName + '</p>';
+
+    const jobLocation = document.querySelector('#jobLocationInput').value  
+    document.getElementById('jobLocationDiv').innerHTML +='<p>' + jobLocation + '</p>';
+
+    const postingWeb = document.querySelector('#jobLocationInput').value  
+    document.getElementById('postingWebDiv').innerHTML += '<p>' + postingWeb + '</p>';
+    
+    const jobTitle = document.querySelector('#jobTitleInput').value  
+    document.getElementById('jobTitleDiv').innerHTML += '<p>' + jobTitle + '</p>';
+
+   
+    // Resets form.
+    $('#form')[0].reset();
+
+}
