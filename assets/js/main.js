de59e21035ad7fbff7c88c42beec8bfd5a9e4532
robert Fialho <32443950+Robydoidao2006@users.noreply.github.com>
2018-11-08 10:03:58 -0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,24 @@
-// Global Variables
-
-// Setting up Date
-const todayTime = new Date();
-const month = (todayTime .getMonth() + 1);
-const day = ('0' + todayTime .getDate()).slice(-2);
-const year = (todayTime .getFullYear());
-const hour = (todayTime .getHours());
-const min = ('0' + todayTime.getMinutes()).slice(-2);
-// Concatinating dates to single string.
-const currentDate = month + "/" + day + "/" + year + " " + hour +":"+ min;
-
-
 // Runs app
 function display(){
-
+    
+    timeNow();
     grabInput();
+}
+
+// Setting up Date
+function timeNow(){
+
+    const todayTime = new Date();
+    const month = (todayTime .getMonth() + 1);
+    const day = ('0' + todayTime .getDate()).slice(-2);
+    const year = (todayTime .getFullYear());
+    const hour = (todayTime .getHours());
+    const min = ('0' + todayTime.getMinutes()).slice(-2);
+    const sec = (todayTime.getSeconds());
+    // Concatinating date to single string.
+    const currentDate = month + "/" + day + "/" + year + " " + hour +":"+ min + " " +sec;
+
+    return currentDate;
 }
 
 function grabInput (){
@@ -24,19 +28,19 @@ function grabInput (){
     const postingWeb = document.querySelector('#postingWebInput').value 
     const jobTitle = document.querySelector('#jobTitleInput').value 
 
-    const table=document.getElementById("results");
-        const row=table.insertRow(-1);
-        const cell1=row.insertCell(0);
-        const cell2=row.insertCell(1);
-        const cell3=row.insertCell(2);
-        const cell4=row.insertCell(3);
-        const cell5=row.insertCell(4);
+    const table = document.getElementById("results");
+        const row = table.insertRow(-1);
+        const cell1 = row.insertCell(0);
+        const cell2 = row.insertCell(1);
+        const cell3 = row.insertCell(2);
+        const cell4 = row.insertCell(3);
+        const cell5 = row.insertCell(4);
 
-        cell1.innerHTML=companyName;
-        cell2.innerHTML=jobLocation;        
-        cell3.innerHTML=postingWeb; 
-        cell4.innerHTML=jobTitle; 
-        cell5.innerHTML= currentDate;
+        cell1.innerHTML = companyName;
+        cell2.innerHTML = jobLocation;        
+        cell3.innerHTML = postingWeb; 
+        cell4.innerHTML = jobTitle; 
+        cell5.innerHTML = timeNow();
 
     // Resets form.
     $('#form')[0].reset();
