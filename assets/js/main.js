242f40aa12766f4565170aec3b82d8df8b66906b
robert Fialho <32443950+Robydoidao2006@users.noreply.github.com>
2018-11-10 15:58:29 -0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 // Global Variables
 var postingArray = [];
+let i = 0;
 
 // Loads local data first
 function init() {
@@ -7,20 +8,18 @@ function init() {
     if (localStorage.postingRecord) {
         postingArray = JSON.parse(localStorage.postingRecord);
         for (var i = 0; i < postingArray.length; i++) {
-            postInput(postingArray[i].companyName,
-            postingArray[i].jobLocation, postingArray[i].postingWeb, 
-            postingArray[i].jobTitle, postingArray[i].time);
+            postInput(
+            postingArray[i].numberId, 
+            postingArray[i].companyName,
+            postingArray[i].jobLocation,
+            postingArray[i].postingWeb, 
+            postingArray[i].jobTitle,
+            postingArray[i].time
+            );
 
         }
     }
 }
-
-// Runs app
-// function display(){
-//     timeNow();
-//     grabInput();  
-//     postInput();
-// }
 
 // Setting up Date
 function timeNow(){
@@ -37,34 +36,47 @@ function timeNow(){
     return currentDate;
 }
 
+
+function numberIdNow(){
+  
+    let currentNumberId = document.querySelector('#inc').value = ++i;
+    return currentNumberId;
+}
+
 function grabInput (){
 
     const companyName = document.querySelector('#companyNameInput').value; 
     const jobLocation = document.querySelector('#jobLocationInput').value 
     const postingWeb = document.querySelector('#postingWebInput').value 
     const jobTitle = document.querySelector('#jobTitleInput').value 
+    const numberId = numberIdNow();
     const time = timeNow();
-    var postingObj = {companyName: companyName, jobLocation: jobLocation, postingWeb: postingWeb, jobTitle: jobTitle, time: time };
+    
+
+    var postingObj = {numberId: numberId, companyName: companyName, jobLocation: jobLocation, postingWeb: postingWeb, jobTitle: jobTitle, time: time };
         postingArray.push(postingObj);
         localStorage.postingRecord = JSON.stringify(postingArray);
-        postInput(companyName, jobLocation, postingWeb, jobTitle, time);
+        postInput(numberId, companyName, jobLocation, postingWeb, jobTitle, time);
         document.getElementById("companyNameInput").value = "";
         document.getElementById("jobLocationInput").value = "";
         document.getElementById("postingWebInput").value = "";
         document.getElementById("jobTitleInput").value = "";
-        document.getElementById(time).value = "";
-
+        (time).value = "";
 }
 
 
-function postInput(companyName, jobLocation, postingWeb, jobTitle, time){
+function postInput( numberId , companyName, jobLocation, postingWeb, jobTitle, time){
     const table = document.getElementById("results");
     const row = table.insertRow(1);
-    const companyNameCell = row.insertCell(0);
-    const jobLocationCell = row.insertCell(1);
-    const postingWebCell = row.insertCell(2);
-    const jobTitleCell = row.insertCell(3);
-    const timeCell = row.insertCell(4);
+
+    const numberIdCell = row.insertCell(0);
+    const companyNameCell = row.insertCell(1);
+    const jobLocationCell = row.insertCell(2);
+    const postingWebCell = row.insertCell(3);
+    const jobTitleCell = row.insertCell(4);
+    const timeCell = row.insertCell(5);
+
+    numberIdCell.innerHTML = numberId;
     companyNameCell.innerHTML = companyName;
     jobLocationCell.innerHTML = jobLocation;        
     postingWebCell.innerHTML = postingWeb; 
